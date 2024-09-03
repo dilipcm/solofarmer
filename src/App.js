1ad9637f7f4eeaec94a35d5c21c23b1d1c307b21
dilipcm <cmdilip2@gmail.com>
2024-09-03 20:38:49 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import ProductScreen from "./components/ProductListing/ProductScreen";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import './App.css';
+// components
+import Toggle from './Navbar/Toggle';
+import Menu from './Navbar/Menu';
+// pages
+import HomePage from './Pages/ContactPage'
+import ExamplePage from './Pages/ExamplePage';
+import ContactPage from './Pages/ContactPage'
+import ProductScreen from './components/ProductListing/ProductScreen'
+function App() {
 
-function App(){
-    return (
-    <div>
-        <h1 style={{textAlign:"center"}}>SOLOFARMER</h1>
-        <ProductScreen />
+  const [navToggled, setNavToggled] = useState(false);
+
+  const handleNavToggle = () => {
+    setNavToggled(!navToggled);
+  }
+
+  return (
+    <div className="App">
+      <Toggle handleNavToggle={handleNavToggle}/>
+      <Router>
+      { navToggled ? <Menu handleNavToggle={handleNavToggle} /> : null }
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/example" component={ExamplePage} />
+          <Route exact path="/contact" component={ContactPage} />
+        </Switch>
+      </Router>
+
+      <ProductScreen /> 
     </div>
-    );
+  );
 }
 
 export default App;
